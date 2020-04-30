47e61ced6aabf40d01380997ae72d58cf7b9b99b
jeffreyadamo <58490053+jeffreyadamo@users.noreply.github.com>
2020-04-29 22:26:09 -0700

    prompt answers are now directed as template literals to the const readMe and written as a markdown with proper template format

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ inquirer.prompt([
   {
     type: "checkbox",
     message: "Apply any of these badges?",
-    name: "stack",
+    name: "badges",
     choices: [
       "HTML", 
       "CSS", 
@@ -75,23 +75,117 @@ inquirer.prompt([
       "MySQL"
     ]
   },
-  //Questions
+  //GitHub username
   {
     type: "input",
-    name: "GitHub",
+    name: "gitHub",
     message: "What is GitHub username to associate this project to?"
+  },
+  //email
+  {
+    type: "input",
+    name: "email",
+    message: "What's your email?"
   },
 ]).then(function(data) {
 
 //   var filename = data.name.toLowerCase().split(' ').join('') + ".json";
 
-  fs.writeFile("readTestMe.md", JSON.stringify(data, null, '\t'), function(err) {
+///////////// THIS WILL WRITE TO A MARKDOWN IN JSON////////////////////
+//   fs.writeFile("readTestMe.md", JSON.stringify(data, null, '\t'), function(err) {
 
-    if (err) {
-      return console.log(err);
-    }
-
+//     if (err) {
+//       return console.log(err);
+//     }
+    // console.log("Success!");
+//   });
+////////////////////////////////////////////////////////////////////////
+//  We can parse out the results and put them in variables
     console.log("Success!");
 
-  });
+    console.log(data.title);
+    
+    const title = data.title;
+    const description = data.description;
+    const installation = data.installation;
+    const usage = data.usage;
+    const contributing = data.contributing;
+    const license = data.license;
+    const tests = data.tests;
+    const gitHub = data.gitHub;
+    const badges = data.badges;
+
+    //can I insert the markdown template here and just insert the variables?
+
+    readMe = 
+`# ${data.title}
+
+### Jeffrey Adamo  
+UW Full Stack BootCamp  
+[${data.title}](#) at GitHub Pages  
+(date)
+***
+
+## Badges go here
+
+Input from index.js: ${data.badges}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+Please run the following command to install dependencies:
+
+
+${data.installation}
+
+
+## Usage
+
+${data.usage}
+
+## License
+
+This projects is license under the ${data.license} license.
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+
+${data.tests}
+
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact [${data.gitHub}](http://www.github.com/${data.gitHub}) or contact directly at ${data.email}. `;
+
+
+    fs.writeFile("readTestMe2.md", readMe, function(err) {
+
+            if (err) {
+              return console.log(err);
+            }
+            console.log("OMG");
+          });
 });
---
+++ readTestMe2.md
@@ -0,0 +1,61 @@
+# test3
+
+### Jeffrey Adamo  
+UW Full Stack BootCamp  
+[test3](#) at GitHub Pages  
+(date)
+***
+
+## Badges go here
+
+Input from index.js: HTML
+
+## Description
+
+trying to ge the markdown formatting unindented
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+Please run the following command to install dependencies:
+
+
+
+
+
+## Usage
+
+
+
+## License
+
+This projects is license under the Copyright license.
+
+## Contributing
+
+
+
+## Tests
+
+To run tests, run the following command:
+
+
+many
+
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact [jeffreyadamo](http://www.github.com/jeffreyadamo) or contact directly at jeffreyadamo@gmail.com. 
